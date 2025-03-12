01c63e0b7d4cdada28caf4f76e14a396e00e0878
Chans <imagechans@gmail.com>
2025-03-12 17:48:48 +0800

    update

--- agent/views.py
+++ agent/views.py
@@ -1,5 +1,7 @@
 import json
 
+from rest_framework.decorators import action
+
 from utils.permissions import IsAuthenticatedExternal
 from .serializers import AgentInputSerializer
 from agent.manager import initialize
@@ -65,4 +67,56 @@ class AgentViewSet(CreateModelMixin,
                 }
             })
 
-        
+    @action(detail=False, methods=['post'])
+    def emotion(self, request):
+        serializer = AgentInputSerializer(data=request.data)
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        # 获取验证后的数据
+        validated_data = serializer.validated_data
+        user_id = request.remote_user.get('id')
+        assistant_name = validated_data.get("assistant_name", 'emotion')
+        model_name = validated_data.get("model_name")
+        users_input = validated_data.get("users_input")
+        language = validated_data.get("language")
+
+        manager = initialize()
+        manager.assistants[assistant_name].set_model(manager.models[model_name])
+
+        # 获取响应内容
+        response_content = manager.invoke(user_id=user_id,
+                                          assistant_name=assistant_name,
+                                          user_input=users_input,
+                                          language=language)
+
+
+        if response_content:  # 确保响应内容不为空
+            try:
+                # 尝试解析JSON
+                parsed_content = json.loads(response_content)
+                return Response({
+                    "status": "success",
+                    "message": "请求已接收",
+                    "data": {
+                        "content": parsed_content
+                    }
+                })
+            except json.JSONDecodeError:
+                # 如果不是有效的JSON，返回原始内容
+                return Response({
+                    "status": "success",
+                    "message": "请求已接收",
+                    "data": {
+                        "content": response_content
+                    }
+                })
+        else:
+            # 处理空响应
+            return Response({
+                "status": "success",
+                "message": "请求已接收",
+                "data": {
+                    "content": {}
+                }
+            })
